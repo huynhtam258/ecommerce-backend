96a4e84f7c7c327118890e930ec90ee675e5598c
Huynh Manh Tam <huynhtam258@gmail.com>
2023-02-25 15:24:09 +0700

    Section_3: env

--- src/app.js
+++ src/app.js
@@ -1,3 +1,4 @@
+require('dotenv').config({ path: `.env.${process.env.NODE_ENV}` })
 const compression = require('compression');
 const express = require('express');
 const { default: helmet } = require('helmet');
--- src/dbs/init.mongodb.js
+++ src/dbs/init.mongodb.js
@@ -1,6 +1,8 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
+const { db: { host, name, port } } = require('./../configs/config.mongodb');
 
-const connectString = `mongodb://localhost:27017/shopDev`
+const connectString = `mongodb://${host}:${port}/${name}`;
+console.log(connectString);
 class Database {
   constructor() {
     this.connect();
