12bdaf4bb1993b8414a6eefa4e218777c0d1483e
Huynh Manh Tam <huynhtam258@gmail.com>
2023-04-22 21:40:25 +0700

    Update auth api document

--- src/routes/access/index.js
+++ src/routes/access/index.js
@@ -7,43 +7,47 @@ const router = express.Router();
 
 /**
  * @swagger
- *   /v1/api/shop/signUp:
- *     post:
- *       summary: Shop Register
- *       tags: [Auth]
- *       security: []
- *     consumes:
- *       - application/json
- *     produces:
- *       - application/json
- *     parameters:
- *       - in: body
- *         name: body
- *         description: Login Shop
- *         required: true
- *         schema:
- *           type: object
- *           properties:
- *             name:
- *               type: string
- *             email:
- *               type: string
- *               format: email
- *             password:
- *               type: string
- *       - in: header
- *         name: x-api-key
- *         type: string
+ * 
+ * /v1/api/shop/signup:
+ *   post:
+ *     summary: Sign Up to the system
+ *     description: Sign Up with email and password
+ *     tags:
+ *       - Auth
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               name:
+ *                 type: string
+ *                 description: Shop Name
+ *               email:
+ *                 type: string
+ *                 format: email
+ *                 description: The user's email address
+ *                 example: kakashi_shop04@gmail.com
+ *               password:
+ *                 type: string
+ *                 description: The user's password
+ *                 example: 123456789
  *     responses:
- *       responses:
- *         "400":
- *           $ref: '#/components/responses/400'
- *         "401":
- *           $ref: '#/components/responses/401'
- *         "200":
- *           description: Register Success
- *           contents:
- *             application/json
+ *       '200':
+ *         description: Login successful
+ *       '400':
+ *         description: Invalid request
+ *       '401':
+ *         description: Unauthorized
+ *     security:
+ *       - ApiKeyAuth: []
+ *     parameters:
+ *       - name: x-api-key
+ *         in: header
+ *         required: true
+ *         description: API key for authentication
+ *         type: string
  */
 router.post('/shop/signup', asyncHandler(accessController.signUp));
 /**
@@ -87,14 +91,77 @@ router.post('/shop/signup', asyncHandler(accessController.signUp));
  *         description: API key for authentication
  *         type: string
  */
- 
 router.post('/shop/login', asyncHandler(accessController.login));
 
 // authentication 
 router.use(authenticationV2);
-// logout
+/**
+ * @swagger
+ * /v1/api/shop/logout:
+ *   post:
+ *     summary: Logout to the system
+ *     description: Login with email and password
+ *     tags:
+ *       - Auth
+ *     responses:
+ *       '200':
+ *         description: Login successful
+ *       '400':
+ *         description: Invalid request
+ *       '401':
+ *         description: Unauthorized
+ *     security:
+ *     parameters:
+ *       - name: x-api-key
+ *         in: header
+ *         required: true
+ *         description: API key for authentication
+ *         type: string
+ *       - name: Authentication
+ *         in: header
+ *         required: true
+ *         description: Token for authentication
+ *         type: string
+ *       - name: x-client-id
+ *         in: header
+ *         required: true
+ *         description: shopId for authentication
+ *         type: string
+ */
 router.post('/shop/logout', asyncHandler(accessController.logout));
-// handler refreshToken
+/**
+ * @swagger
+ * /v1/api/shop/handlerRefreshToken:
+ *   post:
+ *     summary: handlerRefreshToken to the system
+ *     description: Login with email and password
+ *     tags:
+ *       - Auth
+ *     responses:
+ *       '200':
+ *         description: Login successful
+ *       '400':
+ *         description: Invalid request
+ *       '401':
+ *         description: Unauthorized
+ *     security:
+ *     parameters:
+ *       - name: x-api-key
+ *         in: header
+ *         required: true
+ *         description: API key for authentication
+ *         type: string
+ *       - name: Authentication
+ *         in: header
+ *         required: true
+ *         description: Token for authentication
+ *         type: string
+ *       - name: x-client-id
+ *         in: header
+ *         required: true
+ *         description: shopId for authentication
+ *         type: string
+ */
 router.post('/shop/handlerRefreshToken', asyncHandler(accessController.handlerRefreshToken));
 
 module.exports = router;
