d4bf983b969cbfc46372b480658e7d309e598496
Huynh Manh Tam <huynhtam258@gmail.com>
2023-08-26 10:15:58 +0700

    feat: message queue TTL

---
+++ src/test/message_queue/rabbitmq/producerDLX.js
@@ -0,0 +1,47 @@
+const amqp = require('amqplib');
+const message = 'new a product: Title JS'
+
+const runProducer = async () => {
+    try {
+        const connection = await amqp.connect('amqp://guest:guest@localhost')
+        const channel = await connection.createChannel()
+        const notificationExchange = 'notificationEx' // notificationEx direct
+        const notiQueue = 'notificationQueueProcess' // assertQueue
+        const notificationExchangeDLX = 'notificationExDLX' // notificationEx direct
+        const notificationRoutingKeyDLX = 'notificationExDLX' //assert
+
+        // 1. create exchange
+        await channel.assertExchange(notificationExchange, 'direct', {
+            durable: true // khi server chết message vẫn còn trong hàng đợi
+        })
+
+        // 2. create queue
+        // khi một tin nhắn hết hạn thì nó sẽ gửi đến hàng đợi notificationExchangeDLX bằng key notificationRoutingKeyDLX
+        const queueResult = await channel.assertQueue( notiQueue, {
+            exclusive: false, // cho phép các kết nối truy cập vào cùng 1 hàng đợi
+            deadLetterExchange: notificationExchangeDLX,
+            deadLetterRoutingKey: notificationRoutingKeyDLX
+        })
+        // 3. bindQueue 
+        // liên kết giữa queue name và change notification exchange
+        await channel.bindQueue(queueResult.queue, notificationExchange)
+
+        // 4. send message
+        const msg = 'a new product'
+        console.log('producer msg::', msg)
+        await channel.sendToQueue(queueResult.queue, Buffer.from(msg), {
+            expiration: '10000'
+        })
+
+
+
+        setTimeout(() => {
+            connection.close();
+            process.exit(0)
+        }, 500);
+    } catch (error) {
+        console.error(error)
+    }
+}
+
+runProducer().catch(error => console.error(error))
