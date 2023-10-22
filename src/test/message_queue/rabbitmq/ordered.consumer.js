df0a7fb4085b88b07cc6cdfd81b7bc6bca7b9224
Huynh Manh Tam <huynhtam258@gmail.com>
2023-10-22 17:15:59 +0700

    feat: handle prefetch for producer and consumer rabbitmq

---
+++ src/test/message_queue/rabbitmq/ordered.consumer.js
@@ -0,0 +1,25 @@
+const amqp = require('amqplib')
+
+async function consumerOrderedMessage() {
+    const connection = await amqp.connect('amqp://guest:guest@localhost')
+    const channel = await connection.createChannel()
+
+    const queueName = 'ordered-queued-message'
+    await channel.assertQueue(queueName, {
+        durable: true
+    })
+
+    // set prefetch to 1 to ensure only one ack at a time
+    channel.prefetch(1)
+
+    channel.consume(queueName, msg => {
+        const message = msg.content.toString()
+
+        setTimeout(() => {
+            console.log('processed:', message);
+            channel.ack(msg)
+        }, Math.random() * 1000);
+    })
+}
+
+consumerOrderedMessage().catch( err => console.error(err))
